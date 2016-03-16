64a6f14865be193e26a5f2480c1d4fe9b2c67c83
Chase Brasfield <chasebrasfield@gmail.com>
2016-03-16 14:18:30 -0500

    Project Complete

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,11 +2,56 @@
 
 
 var controllers = angular.module('myApp.controllers', []);
-controllers.controller('WelcomeController', ['$scope', function($scope) {
+controllers.controller('WelcomeController', ['$scope', '$rootScope', '$http', function($scope,$rootScope,$http) {
+
+
+    $scope.logIn = function() {
+        var userLogName = $('#login').val();
+        $rootScope.userName = userLogName;
+        if ($('#login').val().length === 0) {
+            alert('You did not enter a tweet to post!');
+            return;
+        } else {
+            window.location.href = "#/tweets";
+        };
+    };
+
+
 
 }]);
 
-controllers.controller('TweetsController', ['$scope', function($scope) {
-    $scope.username = (userName);
-    $scope.userTweet = (tweetList);
+controllers.controller('TweetsController', ['$scope','$rootScope', '$http', function($scope,$rootScope, $http) {
+    console.log($rootScope.username);
+    
+    $scope.getData = function() {
+        $http({
+            method: 'GET',
+            url: '/messages'
+        }).success(function(list) {
+            console.log(list);
+            var tweetList = list;
+            $scope.tweets = tweetList;
+            console.log(tweetList);
+        });
+    };
+    
+    $scope.getData();
+
+    $scope.postData = function() {
+        var user = ($rootScope.username + ': ');
+        var incTweet = {
+            text: $('#new-tweet').val(),
+            username: $rootScope.userName
+        };
+        console.log(incTweet);
+        $http({
+            method: 'POST',
+            url: '/messages',
+            data: incTweet
+        }).success(function() {
+            $scope.getData();
+        });
+
+    };
+
 }]);
--- client/js/main.js
+++ client/js/main.js
@@ -1,29 +1,47 @@
   document.addEventListener("DOMContentLoaded", function(event) {
-    getData();
+
   });
 
-function getUsername() {
-    var userName = $('#login').val();
-    window.userName = userName;
-    if ($('#login').val().length === 0) {
-        alert('You did not enter a tweet to post!');
-        return;
-    } else {
-        window.location.href = "#/tweets";
-    }
-};
+// function getUsername() {
+//     var userName = $('#login').val();
+//     window.userName = userName;
+//     if ($('#login').val().length === 0) {
+//         alert('You did not enter a tweet to post!');
+//         return;
+//     } else {
+//         window.location.href = "#/tweets";
+//     }
+// };
 
 
-function getData() {
-    $.ajax({
-        method: 'GET',
-        url: 'messages',
-    }).done(function(list) {
-        var tweetList = JSON.stringify(list);
-        window.tweetList = tweetList;
-        console.log(tweetList);
-    });
-};
+// function getData() {
+//     $.ajax({
+//         method: 'GET',
+//         url: 'messages',
+//     }).done(function(list) {
+//         var tweetList = JSON.stringify(list);
+//         window.tweetList = tweetList;
+//         console.log(tweetList);
+//     });
+// };
+
+// function postData() {
+//     // console.log('1');
+//     var user = (userName +': ');
+//     var incTweet = {
+//         text: $('#new-tweet').val(),
+//         userName: userName,
+//     };
+//     console.log(incTweet);
+//     $.ajax({
+//         method: 'POST',
+//         url: 'messages',
+//         data: JSON.stringify(incTweet)
+//     }).success(function() {
+//         $('.incoming-tweets').prepend('<li>' + user + ' ' + incTweet.text + '</li>')
+//     });
+//     $('#new-tweet').val('');
+// }
 
 
 var app = angular.module('myApp', ['myApp.controllers', 'ngRoute']);
